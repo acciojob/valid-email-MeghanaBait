f2f76cc93c40839705a030390e4724749b957082
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-22 21:01:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function validEmail(str) {
   //your JS code here.
+	 // Regular expression pattern for validating email addresses
+  const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  
+  // Check if the string matches the pattern
+  return pattern.test(str);
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
